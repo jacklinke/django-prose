9e2dc8ad46f9151fc4b9fb31cc9ff9ec3c8c330e
Jack Linke <jacklinke@gmail.com>
2023-07-30 23:32:00 -0400

    Update to match original project approach to uploads

--- prose/views.py
+++ prose/views.py
@@ -1,30 +1,27 @@
-import filetype
+from django.core.files.storage import default_storage
+from django.http import JsonResponse
 
 from uuid import uuid4
 from datetime import datetime
 
-from django.core.files.storage import default_storage
-from django.http import JsonResponse
+from django.conf import settings
 
-from prose import settings
+ALLOWED_FILE_SIZE = getattr(settings, "PROSE_ATTACHMENT_ALLOWED_FILE_SIZE", 5)
 
 
 def validate_file(file):
-    file_type = filetype.guess(file).mime
     file_size = file.size / 1024 / 1024
-    if (
-        file_type in settings.ALLOWED_ATTACHMENT_FILE_TYPES
-        and file_size <= settings.ALLOWED_ATTACHMENT_FILE_SIZE
-    ):
-        return True
-    return False
+    return file_size <= ALLOWED_FILE_SIZE
 
 
 def upload_attachment(request):
     if request.method == "POST":
         attachment = request.FILES["file"]
         if not validate_file(attachment):
-            return JsonResponse({"error": "File type or size not allowed"}, status=400)
+            return JsonResponse(
+                {"error": f"Files must be {ALLOWED_FILE_SIZE}MB or smaller."},
+                status=400,
+            )
         key = f"{datetime.now().strftime('%Y/%m/%d')}/{uuid4()}.{attachment.name.split('.')[-1]}"
         path = f"prose/{key}"
         default_storage.save(path, attachment)
@@ -32,3 +29,4 @@ def upload_attachment(request):
             "url": default_storage.url(path),
         }
         return JsonResponse(payload, status=201)
+
